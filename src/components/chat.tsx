54d68f43ab1d4af2dc82e746b543b509a044df5b
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-18 20:19:06 -0300

    feat: Add react-markdown dependency and enable markdown rendering in chat messages

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -10,29 +10,44 @@ import {
   CardTitle,
 } from "./ui/card";
 import { useChat } from "ai/react";
-import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { ScrollArea } from "./ui/scroll-area";
-import {Github} from "lucide-react"
+import { Github } from "lucide-react";
+import Markdown from "react-markdown";
+import { Textarea } from "./ui/textarea";
+import { useEffect, useRef } from "react";
 
 const Chat = () => {
-  const { messages, input, handleInputChange, handleSubmit  } = useChat({});
+  const { messages, input, handleInputChange, handleSubmit } = useChat({});
+  const textareaEl = useRef<HTMLTextAreaElement>(null);
+
+  useEffect(() => {
+    if(textareaEl.current) {
+      textareaEl.current.style.height = "0px";
+      const scrollHeight = textareaEl.current.scrollHeight;
+      textareaEl.current.style.height = scrollHeight + "px";
+    }
+  }, [textareaEl, input])
 
   return (
     <Card className="w-full  text-slate-50 bg-transparent border-none lg:w-[32rem] lg:bg-neutral-700 md:bg-neutral-700 lg:border lg:shadow-2xl md:max-w-[26rem] md:border md:shadow-2xl">
       <CardHeader className="relative ">
-          <CardTitle className="text-center">Chat AI
+        <CardTitle className="text-center">
+          Chat AI
           <div className="absolute right-1 top-6 pr-2">
-            <a href="https://github.com/ArthurWillameBr/ChatbotWithGemini" target="_blank">
-              <Github/>
+            <a
+              href="https://github.com/ArthurWillameBr/ChatbotWithGemini"
+              target="_blank"
+            >
+              <Github />
             </a>
           </div>
-          </CardTitle>
+        </CardTitle>
         <CardDescription className="text-center text-slate-100">
           Chatbot feito com Gemini
         </CardDescription>
       </CardHeader>
-      <CardContent >
+      <CardContent>
         {messages.length === 0 ? (
           <div className="flex flex-col gap-2 items-center justify-center h-[60vh] space-y-5 md:max-h-[64vh]">
             <Avatar>
@@ -52,7 +67,10 @@ const Chat = () => {
                   {message.role === "user" && (
                     <Avatar>
                       <AvatarFallback>AW</AvatarFallback>
-                      <AvatarImage  className="size-10" src="https://github.com/Cat7373.png" />
+                      <AvatarImage
+                        className="size-10"
+                        src="https://github.com/Cat7373.png"
+                      />
                     </Avatar>
                   )}
                   {message.role === "assistant" && (
@@ -65,7 +83,8 @@ const Chat = () => {
                     <span className="block font-bold text-slate-50">
                       {message.role === "user" ? "Usuário" : "AI"}
                     </span>
-                    {message.content}
+
+                    <Markdown>{message.content}</Markdown>
                   </p>
                 </div>
               );
@@ -75,12 +94,12 @@ const Chat = () => {
       </CardContent>
       <CardFooter>
         <form
-          className="w-full flex gap-2 items-center  "
+          className="w-full flex gap-2 items-center whitespace-pre-wrap"
           onSubmit={handleSubmit}
         >
-          <Input
+          <Textarea
+            ref={textareaEl}
             placeholder="Pergunte ao chatbot"
-            className="text-black"
             value={input}
             onChange={handleInputChange}
           />
