7128175b6d00bdc3652b2c968373a7d5dbc8c82d
arthurwillame5 <arthurwillame2005@gmail.com>
2024-06-18 20:00:38 -0300

    feat: optimize chatbot prompt generation

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -3,25 +3,20 @@ import { GoogleGenerativeAIStream, Message, StreamingTextResponse } from 'ai';
 
 const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY || '');
 
-const addContextToPrompt = (prompt: string) => {
-  return `" ${prompt}`
-}
-
 const buildGoogleGenAIPrompt = (messages: Message[]) => ({
   contents: messages
     .filter(message => message.role === 'user' || message.role === 'assistant')
     .map(message => ({
       role: message.role === 'user' ? 'user' : 'model',
-      parts: [{ text: addContextToPrompt(message.content) }],
+      parts: [{ text: message.content }],
     })),
 });
 
 export async function POST(req: Request) {
-  
   const { messages } = await req.json();
 
   const geminiStream = await genAI
-    .getGenerativeModel({ model: 'gemini-pro'  })
+    .getGenerativeModel({ model: 'gemini-pro' })
     .generateContentStream(buildGoogleGenAIPrompt(messages));
 
   const stream = GoogleGenerativeAIStream(geminiStream);
